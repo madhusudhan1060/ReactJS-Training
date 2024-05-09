dd27dc1b7744eef9422e5709e1020a01389f848e
madhusudhan boddu <madhusudhan.boddu@microfocus.com>
2024-05-09 21:24:21 +0530

    fetch API data

--- src/components/MainWrapper.js
+++ src/components/MainWrapper.js
@@ -1,6 +1,8 @@
+import React, {useEffect, useState} from "react";
 import RestaurentCard from "./RestaurentCard";
 import styled from "styled-components";
-import resList from "./utils/static/mockdata";
+import resList from "../utils/mockdata";
+import Shimmer from "./Shimmer";
 
 const Button = styled.button`
   /* Adapt the colors based on primary prop */
@@ -13,18 +15,45 @@ const Button = styled.button`
   border-radius: 3px;
 `;
 
-const MainWrapper = () => {
+const MainWrapper = () => {    
+    const [filteredRes, setFilteredRes] = useState([]);
+
+    useEffect(() =>{
+        fetchData();
+        console.log("useEffect");
+    }, []);
+
+    const fetchData = async () => {
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.4107978&lng=78.341552&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const json = await data.json();
+        console.log(json);
+        // setFilteredRes(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
+        // setFilteredRes([]);
+    }
+
+    
+
+    const handleClick = () => {
+        let highRatedRes = resList.filter(res => res.info.avgRating > 4.5);
+        setFilteredRes(highRatedRes);
+    }
+
+    // conditional rendering
+    // if(filteredRes.length === 0) {
+    //     return <h1>Loading....</h1>
+    // }
+    
+
     return (
         <div className="container-fluid px-5 mt-3 main-wrapper">
             <div className="input-group mb-3">
                 <input type="text" className="form-control border border-secondary" placeholder="Search Restaurent" aria-label="Recipient's username" aria-describedby="basic-addon2" />
-                {/* <button className="btn btn-sm btn-secondary px-3" type="button">Search</button> */}
-                <Button type="button" className="btn btn-sm btn-secondary px-3">Search</Button>
-                {/* <Button $primary>Primary</Button> */}
+                <Button $primary type="button" className="btn btn-sm btn-secondary px-3">Search</Button>
             </div>
+            <Button type="button" className="btn btn-sm" onClick={handleClick}>High Rated Restaurent</Button>
             <div className="d-flex flex-wrap row">
                 {
-                    resList.map((res) => (
+                    (filteredRes.length === 0) ? <Shimmer /> : filteredRes.map((res) => (
                         <RestaurentCard key={res.info.id} resData = {res} />
                     ))
                 }
---
+++ src/components/Shimmer.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+function Shimmer() {
+  return (
+    <>
+        <div className="col-3 p-3 d-flex card-shimmer" style={{height: "300px"}}>
+            <div className="card border border-secondary border-opacity-10 shadow shadow-sm w-100" style={{backgroundColor: "#ddd"}}></div>
+        </div>
+        <div className="col-3 p-3 d-flex card-shimmer" style={{height: "300px"}}>
+            <div className="card border border-secondary border-opacity-10 shadow shadow-sm w-100" style={{backgroundColor: "#ddd"}}></div>
+        </div>
+        <div className="col-3 p-3 d-flex card-shimmer" style={{height: "300px"}}>
+            <div className="card border border-secondary border-opacity-10 shadow shadow-sm w-100" style={{backgroundColor: "#ddd"}}></div>
+        </div>
+        <div className="col-3 p-3 d-flex card-shimmer" style={{height: "300px"}}>
+            <div className="card border border-secondary border-opacity-10 shadow shadow-sm w-100" style={{backgroundColor: "#ddd"}}></div>
+        </div>
+        <div className="col-3 p-3 d-flex card-shimmer" style={{height: "300px"}}>
+            <div className="card border border-secondary border-opacity-10 shadow shadow-sm w-100" style={{backgroundColor: "#ddd"}}></div>
+        </div>
+        <div className="col-3 p-3 d-flex card-shimmer" style={{height: "300px"}}>
+            <div className="card border border-secondary border-opacity-10 shadow shadow-sm w-100" style={{backgroundColor: "#ddd"}}></div>
+        </div>
+        <div className="col-3 p-3 d-flex card-shimmer" style={{height: "300px"}}>
+            <div className="card border border-secondary border-opacity-10 shadow shadow-sm w-100" style={{backgroundColor: "#ddd"}}></div>
+        </div>
+        <div className="col-3 p-3 d-flex card-shimmer" style={{height: "300px"}}>
+            <div className="card border border-secondary border-opacity-10 shadow shadow-sm w-100" style={{backgroundColor: "#ddd"}}></div>
+        </div>
+        <div className="col-3 p-3 d-flex card-shimmer" style={{height: "300px"}}>
+            <div className="card border border-secondary border-opacity-10 shadow shadow-sm w-100" style={{backgroundColor: "#ddd"}}></div>
+        </div>
+        <div className="col-3 p-3 d-flex card-shimmer" style={{height: "300px"}}>
+            <div className="card border border-secondary border-opacity-10 shadow shadow-sm w-100" style={{backgroundColor: "#ddd"}}></div>
+        </div>
+    </>
+  )
+}
+
+export default Shimmer
